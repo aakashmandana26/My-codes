3a1e7f0db698d7f348d73d633781ab44705c4c6b
aakashmandana26 <73030921+aakashmandana26@users.noreply.github.com>
2022-03-06 00:36:26 +0530

    Delete __pycache__ directory

--- __pycache__/pccoe dsa/harvard/main_trial.py
+++
@@ -1,3 +0,0 @@
-import name_main
-print("this is not main")
-print(name_main.fun1("hi this is not main"))
--- __pycache__/pccoe dsa/harvard/reduce.py
+++
@@ -1,8 +0,0 @@
-from functools import reduce
-num = []
-for i in range(10):
-    num.append(i)
-
-sum = reduce((lambda x,y : x+y),num)
-print(sum)
-
